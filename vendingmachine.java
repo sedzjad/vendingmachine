fe6e6f98af54299980207e14b990de537890b90f
sedzjad <31769691+sedzjad@users.noreply.github.com>
2018-11-01 16:59:55 +0100

    Add files via upload
    Removed my first cases
    Made a switch with some cases

--- vendingmachine.java
+++ vendingmachine.java
@@ -1,46 +1,54 @@
 import java.util.*;
 public class vendingmachine {
-    public static void main (String[] args){
-    Scanner scanner = new Scanner(System.in);
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    int Cola = 90;
-    int Fanta = 90;
-    int Sprite = 90;
-    int choice = 0;
-    int money = 0;
-    int amount = 0;
-    int totaalbedrag, centen;
+        int Cola = 90;
+        int Fanta = 90;
+        int Sprite = 90;
+        int choice = 0;
+        int money = 0;
+        int amount = 0;
+        int totaalbedrag, centen;
 
-    ArrayList<String> alist = new ArrayList<String>();
-    alist.add("Cola");
-    alist.add("Fanta");
-    alist.add("Sprite");
+        ArrayList<String> alist = new ArrayList<String>();
+        alist.add("Cola");
+        alist.add("Fanta");
+        alist.add("Sprite");
 
-    System.out.println("We have the following drinks");
-    System.out.println(alist);
+        System.out.println("We have the following drinks");
+        System.out.println(alist);
 
-    System.out.println("Please enter your selection");
-    choice = scanner.nextInt();{
-        
+        System.out.println("Please enter your selection");
+        choice = scanner.nextInt();
+        {
+            System.out.println("Enter your amount in cents");
+            money = scanner.nextInt();
+            if (money > 200) {
+                System.out.println("Please insert an amount lower than that!");
+                money = scanner.nextInt();
+            } else System.exit(0);
+        }
+        switch (choice) {
+            case 1:
+                System.out.println("You've entered:" + money + " cent");
+                System.out.println("You have chosen Cola!");
+                amount = money - Cola;
+                System.out.println("Your change is:" + amount + " cents");
+                break;
+
+            case 2:
+                System.out.println("You've entered:" + money + " cent");
+                System.out.println("You have chosen Fanta!");
+                amount = money - Fanta;
+                System.out.println("Your change is:" + amount + " cent");
+
+            case 3:
+                System.out.println("You've entered:" + money + " cent");
+                System.out.println("You have chosen Sprite!");
+                amount = money - Sprite;
+                System.out.println("Your change is:" + amount + " cent");
         }
 
-    String drankjes;
-            boolean yn = false;
-            System.out.println("What would you like to drink today?");
-            drankjes = scanner.next();
-            switch (drankjes) {
-                case "Cola":
-                    alist.get(0);
-                    System.out.println("Je hebt Cola gekozen");
-                    break;
-                case "Fanta":
-                    alist.get(1);
-                    System.out.println("Je hebt Fanta gekozen");
-                    break;
-                case "Sprite":
-                    alist.get(2);
-                    System.out.println("Je hebt Sprite gekozen");
-                    break;
-            }
     }
 }
